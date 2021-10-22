225706801ddc9787812f961df77294b944978dbf
Tomom1-S <Tomomi.Suzuki1@jp.ricoh.com>
2021-10-22 23:02:57 +0900

    Get all target files

--- projects/07/typescript-src/src/main.ts
+++ projects/07/typescript-src/src/main.ts
@@ -1,3 +1,26 @@
+import * as fs from "fs";
+
 const targetPath = process.argv.slice(2)[0];
 
-// TODO: targetPathがディレクトリの場合、そこに含まれる全ての.vmファイルを処理する
+let targets: string[] = [];
+if (fs.lstatSync(targetPath).isFile()) {
+  if (!isVmFile(targetPath)) {
+    throw new Error("targetPath is invalid");
+  }
+  targets = [targetPath];
+} else {
+  const files = fs.readdirSync(targetPath);
+  const directory = targetPath.endsWith("/") ? targetPath : targetPath + "/";
+  targets = files
+    .filter((file) => isVmFile(file))
+    .map((file) => {
+      return `${directory}${file}`;
+    });
+}
+
+console.log(targets);
+// TODO: targets の各要素に対して処理を実行
+
+function isVmFile(path: string): boolean {
+  return path.split(".").pop() === "vm";
+}
