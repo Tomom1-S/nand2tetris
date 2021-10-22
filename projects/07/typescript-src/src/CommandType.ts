1184a612b5f1dcf3d78d80ce23289fa1fe18c84f
Tomom1-S <Tomomi.Suzuki1@jp.ricoh.com>
2021-10-22 22:59:12 +0900

    Fix CommandType

--- projects/07/typescript-src/src/CommandType.ts
+++ projects/07/typescript-src/src/CommandType.ts
@@ -1,18 +1,28 @@
-export class CommandType {
-  constructor(public value: string) {}
-
-  toString() {
-    return this.value;
-  }
-
-  // values
-  static arithmetic = new CommandType("C_ARITHMETIC");
-  static push = new CommandType("C_PUSH");
-  static pop = new CommandType("C_POP");
-  static label = new CommandType("C_LABEL");
-  static goto = new CommandType("C_GOTO");
-  static if = new CommandType("C_IF");
-  static function = new CommandType("C_FUNCTION");
-  static return = new CommandType("C_RETURN");
-  static call = new CommandType("C_CALL");
-}
+type CommandType =
+  | {
+      name: "C_ARITHMETIC";
+    }
+  | {
+      name: "C_PUSH";
+    }
+  | {
+      name: "C_POP";
+    }
+  | {
+      name: "C_LABEL";
+    }
+  | {
+      name: "C_GOTO";
+    }
+  | {
+      name: "C_IF";
+    }
+  | {
+      name: "C_FUNCTION";
+    }
+  | {
+      name: "C_RETURN";
+    }
+  | {
+      name: "C_CALL";
+    };
