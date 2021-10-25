eb2a500b10f175eecd8f3b6461af411020e2cdec
Tomom1-S <Tomomi.Suzuki1@jp.ricoh.com>
2021-10-25 20:06:33 +0900

    Implement main

--- projects/07/typescript-src/src/main.ts
+++ projects/07/typescript-src/src/main.ts
@@ -1,4 +1,6 @@
 import * as fs from "fs";
+import { CodeWriter } from "./CodeWriter";
+import { Parser } from "./Parser";
 
 const targetPath = process.argv.slice(2)[0];
 
@@ -18,8 +20,38 @@ if (fs.lstatSync(targetPath).isFile()) {
     });
 }
 
-console.log(targets);
-// TODO: targets の各要素に対して処理を実行
+// 対象ファイルがなかったら何もせずに終了
+if (targets === undefined || targets.length == 0) {
+  console.log("No target file found.");
+  process.exit();
+}
+
+for (const target of targets) {
+  const parser = new Parser(target);
+  const writer = new CodeWriter(target);
+  writer.setFileName(target);
+
+  while (parser.hasMoreCommands()) {
+    parser.advance();
+    const commandType = parser.commandType();
+    switch (commandType.name) {
+      case "C_RETURN":
+        break;
+      case "C_ARITHMETIC":
+        writer.writeArithmetic(parser.arg1());
+        break;
+      case "C_POP":
+      case "C_PUSH":
+        writer.writePushPop(commandType.name, parser.arg1(), parser.arg2());
+        break;
+      default:
+        break;
+    }
+  }
+
+  writer.close();
+  console.log(`Finish VM transfer of ${target}`);
+}
 
 function isVmFile(path: string): boolean {
   return path.split(".").pop() === "vm";
