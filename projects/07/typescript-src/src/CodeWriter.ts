8c5f6481ae3a444a75cb2a8bccac5fee4d3ac453
Tomomi Suzuki <39720275+Tomom1-S@users.noreply.github.com>
2021-12-30 16:16:25 +0900

    [Ch07] Project (#9)
    * Add new files for project
    
    * Update main
    
    * lint:fix
    
    * Fix CommandType
    
    * Get all target files
    
    * Export CommandType
    
    * Implement main
    
    * Implement stage#1
    
    * Implement stage#1
    
    * Implement stage#1
    
    * Fix unnecessary log & add comment
    
    * Implement stage#2
    
    * Implement stage#2
    
    * Implement stage#2
    
    * Implement stage#2
    
    * Refactoring
    
    * Fix test error

---
+++ projects/07/typescript-src/src/CodeWriter.ts
@@ -0,0 +1,275 @@
+import * as fs from "fs";
+import * as path from "path";
+
+const SEPARATOR = "\n";
+const POINTER_BASE = 3;
+const TEMP_BASE = 5;
+const POP_STACK = ["@SP", "AM=M-1", "D=M"];
+const PUSH_STACK = ["@SP", "A=M", "M=D", "@SP", "M=M+1"];
+
+export class CodeWriter {
+  fileName: string;
+  saveName: string;
+  labelCount = 0;
+  results: string[] = [];
+
+  constructor(filepath: string) {
+    this.saveName = `${filepath.substr(0, filepath.lastIndexOf("."))}.asm`;
+  }
+
+  setFileName(fileName: string): void {
+    // TODO: CodeWriterモジュールに新しいVMファイルの変換が開始したことを知らせる
+    // ↑どういう意味？？？
+    this.fileName = path.parse(fileName).name;
+  }
+
+  writeArithmetic(command: string): void {
+    switch (command) {
+      case "add":
+        this.results.push(...POP_STACK);
+        this.results.push("@SP");
+        this.results.push("AM=M-1");
+        this.results.push("M=M+D");
+        this.results.push("@SP");
+        this.results.push("M=M+1");
+        return;
+      case "sub":
+        this.results.push(...POP_STACK);
+        this.results.push("@SP");
+        this.results.push("AM=M-1");
+        this.results.push("M=M-D");
+        this.results.push("@SP");
+        this.results.push("M=M+1");
+        return;
+      case "neg":
+        this.results.push("@SP");
+        this.results.push("AM=M-1");
+        this.results.push("M=-M");
+        this.results.push("@SP");
+        this.results.push("M=M+1");
+        break;
+      case "eq": {
+        const count = this.labelCount++;
+        this.results.push(...POP_STACK);
+        this.results.push("@SP");
+        this.results.push("AM=M-1");
+        this.results.push("D=M-D");
+        this.results.push(`@TRUE_${count}`);
+        this.results.push("D;JEQ");
+        this.results.push("@SP");
+        this.results.push("A=M");
+        this.results.push("M=0");
+        this.results.push(`@END_${count}`);
+        this.results.push("0;JMP");
+        this.results.push(`(TRUE_${count})`);
+        this.results.push("@SP");
+        this.results.push("A=M");
+        this.results.push("M=-1");
+        this.results.push(`(END_${count})`);
+        this.results.push("@SP");
+        this.results.push("M=M+1");
+        return;
+      }
+      case "gt": {
+        const count = this.labelCount++;
+        this.results.push(...POP_STACK);
+        this.results.push("@SP");
+        this.results.push("AM=M-1");
+        this.results.push("D=M-D");
+        this.results.push(`@TRUE_${count}`);
+        this.results.push("D;JGT");
+        this.results.push("@SP");
+        this.results.push("A=M");
+        this.results.push("M=0");
+        this.results.push(`@END_${count}`);
+        this.results.push("0;JMP");
+        this.results.push(`(TRUE_${count})`);
+        this.results.push("@SP");
+        this.results.push("A=M");
+        this.results.push("M=-1");
+        this.results.push(`(END_${count})`);
+        this.results.push("@SP");
+        this.results.push("M=M+1");
+        return;
+      }
+      case "lt": {
+        const count = this.labelCount++;
+        this.results.push(...POP_STACK);
+        this.results.push("@SP");
+        this.results.push("AM=M-1");
+        this.results.push("D=M-D");
+        this.results.push(`@TRUE_${count}`);
+        this.results.push("D;JLT");
+        this.results.push("@SP");
+        this.results.push("A=M");
+        this.results.push("M=0");
+        this.results.push(`@END_${count}`);
+        this.results.push("0;JMP");
+        this.results.push(`(TRUE_${count})`);
+        this.results.push("@SP");
+        this.results.push("A=M");
+        this.results.push("M=-1");
+        this.results.push(`(END_${count})`);
+        this.results.push("@SP");
+        this.results.push("M=M+1");
+        return;
+      }
+      case "and":
+        this.results.push(...POP_STACK);
+        this.results.push("@SP");
+        this.results.push("AM=M-1");
+        this.results.push("M=M&D");
+        this.results.push("@SP");
+        this.results.push("M=M+1");
+        return;
+      case "or":
+        this.results.push(...POP_STACK);
+        this.results.push("@SP");
+        this.results.push("AM=M-1");
+        this.results.push("M=M|D");
+        this.results.push("@SP");
+        this.results.push("M=M+1");
+        return;
+      case "not":
+        this.results.push("@SP");
+        this.results.push("AM=M-1");
+        this.results.push("M=!M");
+        this.results.push("@SP");
+        this.results.push("M=M+1");
+        return;
+      default:
+        throw new Error(`Invalid command: ${command}`);
+    }
+  }
+
+  writePushPop(command: string, segment: string, index: number): void {
+    // TODO: C_PUSHまたはC_POPコマンドをアセンブリコードに変換し、それを書き込む
+    // 4. 最後に、static セグメントに対応する。
+    let asm;
+    switch (command) {
+      case "push":
+        asm = convertPush(segment, index, this.fileName);
+        break;
+      case "pop":
+        asm = convertPop(segment, index, this.fileName);
+        break;
+      default:
+        throw new Error(`Invalid command: ${command}`);
+    }
+    this.results.push(...asm);
+  }
+
+  close(): void {
+    // 最後に無限ループを入れてHackプログラムを終了させる
+    this.results.push("(END)");
+    this.results.push("@END");
+    this.results.push("0;JMP");
+
+    fs.writeFile(this.saveName, this.results.join(SEPARATOR), (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  }
+}
+
+function convertPush(
+  segment: string,
+  index: number,
+  fileName: string
+): string[] {
+  if (segment === "constant") {
+    const results = [];
+    results.push(`@${index}`);
+    results.push("D=A");
+    results.push(...PUSH_STACK);
+    return results;
+  }
+
+  const label = createLabel({ segment, index }, fileName);
+  const formula = {
+    forwardAddress: [] as string[],
+    moveTargetAddress: [] as string[],
+  };
+  switch (segment) {
+    case "pointer":
+    case "temp":
+    case "static":
+      break;
+    default:
+      formula.forwardAddress.push(`@${index}`);
+      formula.forwardAddress.push("D=A");
+
+      formula.moveTargetAddress.push("A=M+D");
+  }
+  const results = [];
+  results.push(...formula.forwardAddress);
+  results.push(`@${label}`);
+  results.push(...formula.moveTargetAddress);
+  results.push("D=M");
+  results.push(...PUSH_STACK);
+  return results;
+}
+
+function convertPop(
+  segment: string,
+  index: number,
+  fileName: string
+): string[] {
+  const label = createLabel({ segment, index }, fileName);
+  const formula = {
+    forwardAddress: [] as string[],
+    moveTargetAddress: [] as string[],
+    backAddress: [] as string[],
+  };
+  switch (segment) {
+    case "pointer":
+    case "temp":
+    case "static":
+      break;
+    default:
+      formula.forwardAddress.push(`@${index}`);
+      formula.forwardAddress.push("D=A");
+      formula.forwardAddress.push(`@${label}`);
+      formula.forwardAddress.push("M=M+D");
+
+      formula.moveTargetAddress.push("A=M");
+
+      formula.backAddress.push(`@${index}`);
+      formula.backAddress.push("D=A");
+      formula.backAddress.push(`@${label}`);
+      formula.backAddress.push("M=M-D");
+  }
+  const results = [];
+  results.push(...formula.forwardAddress);
+  results.push(...POP_STACK);
+  results.push(`@${label}`);
+  results.push(...formula.moveTargetAddress);
+  results.push("M=D");
+  results.push(...formula.backAddress);
+  return results;
+}
+
+function createLabel(
+  input: { segment: string; index: number },
+  fileName: string
+): string {
+  switch (input.segment) {
+    case "local":
+      return "LCL";
+    case "argument":
+      return "ARG";
+    case "this":
+      return "THIS";
+    case "that":
+      return "THAT";
+    case "pointer":
+      return `R${POINTER_BASE + input.index}`;
+    case "temp":
+      return `R${TEMP_BASE + input.index}`;
+    case "static":
+      return `${fileName}.${input.index}`;
+    default:
+      throw new Error(`Invalid segment: ${input.segment}`);
+  }
+}
---
+++ projects/07/typescript-src/src/Parser.ts
@@ -0,0 +1,97 @@
+import * as fs from "fs";
+import { CommandType } from "./type";
+
+export class Parser {
+  data: string[];
+  index = 0;
+  command: string[] = [];
+
+  constructor(filePath: string) {
+    const file = fs.readFileSync(filePath, { encoding: "utf8" });
+    this.data = file
+      .toString()
+      .split(/\r?\n/)
+      .filter((line) => line) // 空行を除去
+      .filter((line) => !line.startsWith("//")); // コメント行を削除
+  }
+
+  /**
+   * 入力において、さらにコマンドが存在するか?
+   * @returns コマンドが存在するか
+   */
+  hasMoreCommands(): boolean {
+    if (!this.data) {
+      return false;
+    }
+    return this.index < this.data.length;
+  }
+
+  /**
+   * 入力から次のコマンドを読み、それを現在のコマンドにする
+   */
+  advance(): void {
+    this.command = this.data[this.index++].split(" ");
+  }
+
+  /**
+   * 現VMコマンドの種類を返す。算術コマンドはすべてC_ARITHMETICが返される。
+   * @returns CommandType
+   */
+  commandType(): CommandType {
+    switch (this.command[0]) {
+      case "push":
+        return {
+          name: "C_PUSH",
+          command: "push",
+        };
+      case "pop":
+        return {
+          name: "C_POP",
+          command: "pop",
+        };
+      case "add":
+      case "sub":
+      case "neg":
+      case "eq":
+      case "gt":
+      case "lt":
+      case "and":
+      case "or":
+      case "not":
+        return {
+          name: "C_ARITHMETIC",
+          command: this.command[0],
+        };
+    }
+    throw new Error("Not assigned to any CommandType");
+  }
+
+  /**
+   * 現コマンドの最初の引数が返される
+   * C_ARITHMETICの場合、コマンド自体(add、subなど)が返される。
+   * 現コマンドがC_RETURNの場合、本ルーチンは呼ばないようにする。
+   * @returns 最初の引数
+   */
+  arg1(): string {
+    if (this.commandType().name === "C_ARITHMETIC") {
+      return this.command[0];
+    }
+
+    if (this.command.length < 2) {
+      throw new Error(`No 1st argument found in "${this.command.join(" ")}"`);
+    }
+    return this.command[1];
+  }
+
+  /**
+   * 現コマンドの2番目の引数が返される。
+   * 現コマンドがC_PUSH、C_POP、C_FUNCTION、C_CALLの場合のみ本ルーチンを呼ぶようにする。
+   * @returns 2番目の引数
+   */
+  arg2(): number {
+    if (this.command.length < 3) {
+      throw new Error(`No 2nd argument found in "${this.command.join(" ")}"`);
+    }
+    return parseInt(this.command[2]);
+  }
+}
